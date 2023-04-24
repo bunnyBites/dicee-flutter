09254340b6371fbc50fa54c3eebbcefbc97336f8
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-24 21:03:16 +0530

    add random dice generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,57 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue.shade800,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.blue.shade900,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePage();
+}
+
+class _DicePage extends State<DicePage> {
+  int leftDiceIndex = 1;
+  int rightDiceIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    print("Left button is selected");
-                  },
-                  child: Image.asset("images/dice1.png")),
-            ),
-            Expanded(
-                child: TextButton(
-                    onPressed: () {
-                      print("Right button is pressed");
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () => {
+                      setState(() {
+                        leftDiceIndex = (Random().nextInt(6) + 1);
+                      }),
+                      debugPrint("Left button is selected"),
                     },
-                    child: Image.asset("images/dice2.png"))),
-          ],
-        ),
+                child: Image.asset("images/dice$leftDiceIndex.png")),
+          ),
+          Expanded(
+              child: TextButton(
+                  onPressed: () => {
+                        setState(() {
+                          rightDiceIndex = (Random().nextInt(6) + 1);
+                        }),
+                        debugPrint("Right button is pressed"),
+                      },
+                  child: Image.asset("images/dice$rightDiceIndex.png"))),
+        ],
       ),
     );
   }
